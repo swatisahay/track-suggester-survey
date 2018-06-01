8229cabd9a3f5b46f2ad74eb2416f4e0ba0ce687
Swati Sahay <swatiranjan0111@gmail.com>
2018-06-01 14:08:21 -0700

    added 4 javascript conditions

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+
+$(function() {
+  $("#btn-submit").click(function(){
+    var webdevelopment = parseInt($("input:radio[name=development]:checked").val());
+    var busi = parseInt($("input:radio[name=business]:checked").val());
+    var webdesign = parseInt($("input:radio[name=design]:checked").val());
+    var serverside = parseInt($("input:radio[name=server]:checked").val());
+    var ininteractive = parseInt($("input:radio[name=interactive]:checked").val());
+
+    //backend is selected
+if((!webdevelopment)&&(!busi)&&(!webdesign)&&(!serverside)&&(!ininteractive)){
+           $(".initial-hide").hide();
+           $("#csharp").show();
+         }
+else if((webdevelopment)&&(busi)&&(webdesign)&&(serverside)&&(ininteractive))  {
+      $(".initial-hide").hide();
+      $("#css").show();
+    }
+else if((!webdevelopment)&&(!busi)&&(webdesign)&&(serverside)&&(ininteractive)){
+        $(".initial-hide").hide();
+        $("#ruby").show();
+}
+else if ((!webdevelopment)&&(!busi)&&(!webdesign)&&(serverside)&&(!ininteractive)){
+  $(".initial-hide").hide();
+  $("#php").show();
+}
+
+
+
+
+
+
+
+
+
+  });
+});
