d7667db9931e9f87d167a3d62d485ad24fbb4510
Swati Sahay <swatiranjan0111@gmail.com>
2018-06-01 16:35:04 -0700

    Minor changes made in script

--- js/scripts.js
+++ js/scripts.js
@@ -7,70 +7,47 @@ $(function() {
     var serverside = parseInt($("input:radio[name=server]:checked").val());
     var ininteractive = parseInt($("input:radio[name=interactive]:checked").val());
 
-    //input val= 0,0,0,0,0
+    //backend is selected
     if((!webdevelopment)&&(!busi)&&(!webdesign)&&(!serverside)&&(!ininteractive)){
       $(".initial-hide").hide();
       $("#csharp").show();
     }
-
-    //input val= 1,1,1,1,1
     else if((webdevelopment)&&(busi)&&(webdesign)&&(serverside)&&(ininteractive))  {
       $(".initial-hide").hide();
       alert("Sorry there are no tracks available for you ")
     }
-
-    //input val= 0,0,1,1,1
     else if((!webdevelopment)&&(!busi)&&(webdesign)&&(serverside)&&(ininteractive)){
       $(".initial-hide").hide();
       $("#ruby").show();
     }
-
-    //input val= 0,0,0,1,0
     else if ((!webdevelopment)&&(!busi)&&(!webdesign)&&(serverside)&&(!ininteractive)){
       $(".initial-hide").hide();
       $("#php").show();
     }
-
-    //input val= 0,0,1,0,1
     else if ((webdevelopment)&&(busi)&&(!webdesign)&&(serverside)&&(!ininteractive)) {
       $(".initial-hide").hide();
       $("#css").show();
     }
-
-    //input val= 0,1,0,0,0
     else if ((!webdevelopment)&&(busi)&&(!webdesign)&&(!serverside)&&(!ininteractive)) {
       $(".initial-hide").hide();
       $("#php").show();
     }
 
-    //input val= 0,0,0,0,1
     else if ((!webdevelopment)&&(!busi)&&(!webdesign)&&(!serverside)&&(ininteractive)) {
       $(".initial-hide").hide();
       $("#csharp").show();
     }
-
-    //input val= 0,1,1,0,0
-    else if ((!webdevelopment)&&(busi)&&(webdesign)&&(!serverside)&&(!ininteractive)) {
+    else if ((webdevelopment)&&(busi)&&(webdesign)&&(!serverside)&&(!ininteractive)) {
       $(".initial-hide").hide();
       $("#php").show();
     }
-
-    //input val= 1,0,0,0,0
     else if ((webdevelopment)&&(!busi)&&(!webdesign)&&(!serverside)&&(!ininteractive)) {
       $(".initial-hide").hide();
       $("#ruby").show();
     }
-
-    //input val= 1,1,0,0,0
-    else if((webdevelopment)&&(busi)&&(!webdesign)&&(serverside)&&(!ininterasctive)) {
+    else if((webdevelopment)&&(busi)&&(!webdesign)&&(serverside)&&(!ininteractive)) {
       $(".initial-hide").hide();
       $("#css").show();
-    }
-
-    //input val= 1,1,1,0,0
-    else ((webdevelopment)&&(busi)&&(webdesign)&&(!serverside)&&(!ininterasctive)) {
-      $(".initial-hide").hide();
-      alert("sorry !! Your answers doesnot have any match in our library")
     }
 
   });
